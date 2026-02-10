8e3715bfbe8bd79e998f6a40bf3bc43a62e655f9
Sharath Kumar A G <4mh23cs138@gmail.com>
2026-02-10 04:36:43 +0000

    your commit message

--- main2.py
+++ main2.py
@@ -4,6 +4,14 @@ load_dotenv()
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.agents import create_agent
 
+def primes_up_to(n):
+    primes = []
+    for num in range(2, n + 1):
+        if is_prime(num):
+            primes.append(num)
+    return primes
+
+
 def get_weather(city: str) -> str:
     """Get weather for a given city."""
     return f"It's always sunny in {city}!"
@@ -21,7 +29,7 @@ agent = create_agent(
 
 # Run the agent
 response = agent.invoke(
-    {"messages": [{"role": "user", "content": "what is the weather in sf"}]}
+    {"messages": [{"role": "user", "content": "what is the weather 2001 is prime number"}]}
 )
 
 # Print only the final AI response
